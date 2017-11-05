9c70811712489bebd11f4a5c8a30c980630a468c
Pawel007 <pawel.jakubowski@outlook.com>
2017-11-05 19:15:47 +0100

    Dodaj pliki projektów.

---
+++ Cwiczenie_yt_6/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cwiczenie_yt_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Wpisz swoje Imie: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Witaj, " + name);
+
+            Console.Write("Wpisz ile masz lat: ");
+            // int age = int.Parse(Console.ReadLine());
+            int age;
+            bool result = int.TryParse(Console.ReadLine(), out age);
+
+            if (age > 18)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Mozesz wypic piwo");
+            }
+            else if (result == false)
+            {
+                Console.WriteLine("Wprowadziles nie poprawny wiek");
+            }
+            else
+            {
+                Console.WriteLine("Dostaniesz mleko");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
